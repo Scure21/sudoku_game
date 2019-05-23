94245cf6378b53dd10e6fdf280a4278867730bc9
Stephanie Cure <stephanie.cure@willowtreeapps.com>
2019-05-23 14:41:32 -0400

    Make a Square a class

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,14 +1,27 @@
 import * as React from "react";
 import Square from "./Square"
 
-class Board extends React.Component<{},{}>{
+interface IState {}
+
+interface IProps {}
+
+class Board extends React.Component<IProps, IState>{
+  constructor(props: IProps){
+    super(props);
+  }
+
     render(){
       const rows = [0,1,2,3,4,5,6,7,8];
       const columns = [0,1,2,3,4,5,6,7,8];
 
       return(
         <div className="board-container">
-              {rows.map(row => columns.map(col => <Square value={row} className={`square-${row}-${col}`} key={col}/>))}
+              {rows.map(row => columns.map(col => <Square value={row}
+              className="square"
+              id={`${row}-${col}`}
+              key={col}
+              />))
+              }
         </div>
       );
     }
--- src/components/Square.tsx
+++ src/components/Square.tsx
@@ -1,14 +1,35 @@
 import * as React from "react";
 
+interface IState {
+  class: string;
+}
 interface IProps {
   value: number;
   className: string;
+  id: string;
+  onClick?: () => void;
 }
 
-const Square: React.SFC<IProps> = (props) => (
-        <div className={"square " + props.className}>
-           {props.value}
-        </div>
-        );
-
-  export default Square;
+export default class Square extends React.Component<IProps, IState>{
+  constructor(props: IProps) {
+    super(props);
+    this.state = {
+      class: "off"
+    }
+    this.toggleClass = this.toggleClass.bind(this);
+  }
+  // method to toggle on and off the square class when clicked
+  toggleClass(): void {
+    let css = this.state.class === "on" ? "off" : "on";
+    this.setState({ class: css });
+  }
+  render(){
+    const props = this.props;
+    return (
+    <div className={props.className + " " + this.state.class}
+      onClick={this.toggleClass}
+      id={props.id}>
+      {props.value}
+    </div>)
+  }
+}
